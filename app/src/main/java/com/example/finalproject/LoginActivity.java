141f2543ae81265bd98498b0406071badf36c090
atomspirit <Yoavd246@gmail.com>
2023-11-28 16:57:49 +0200

    connected login to main

--- app/src/main/java/com/example/finalproject/LoginActivity.java
+++ app/src/main/java/com/example/finalproject/LoginActivity.java
@@ -11,38 +11,39 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity implements MotionLayout.TransitionListener {
+public class LoginActivity extends AppCompatActivity {
 
     Button btLogin;
     EditText etUsername;
+    MotionLayout motionLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        motionLayout = findViewById(R.id.motion_layout_login);
+        motionLayout.setTransitionListener(new MotionLayout.TransitionListener() {
+            @Override
+            public void onTransitionStarted(MotionLayout motionLayout, int startId, int endId) {
 
-    }
+            }
 
-    @Override
-    public void onTransitionStarted(MotionLayout motionLayout, int startId, int endId) {
+            @Override
+            public void onTransitionChange(MotionLayout motionLayout, int startId, int endId, float progress) {
 
-    }
+            }
 
-    @Override
-    public void onTransitionChange(MotionLayout motionLayout, int startId, int endId, float progress) {
+            @Override
+            public void onTransitionCompleted(MotionLayout motionLayout, int currentId) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
 
-    }
+            @Override
+            public void onTransitionTrigger(MotionLayout motionLayout, int triggerId, boolean positive, float progress) {
 
-    @Override
-    public void onTransitionCompleted(MotionLayout motionLayout, int currentId) {
-        Toast.makeText(getApplicationContext(),"finished",Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
-    }
-
-    @Override
-    public void onTransitionTrigger(MotionLayout motionLayout, int triggerId, boolean positive, float progress) {
-        Toast.makeText(getApplicationContext(),"trigger",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
