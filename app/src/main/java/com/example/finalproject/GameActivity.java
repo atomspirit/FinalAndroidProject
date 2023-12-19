aba95ec1ed95d70ae90c4d32a376e1041882cb24
atomspirit <Yoavd246@gmail.com>
2023-12-19 12:19:30 +0200

    Added stuff in themes.xml and GameActivity

--- app/src/main/java/com/example/finalproject/GameActivity.java
+++ app/src/main/java/com/example/finalproject/GameActivity.java
@@ -3,32 +3,43 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class GameActivity extends AppCompatActivity {
 
+    ImageView ivAddGame;
+    ListView listView;
+    ArrayList<Game> games;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        ListView listView = findViewById(R.id.lvGameList);
+        ivAddGame = findViewById(R.id.ivIcon2);
+        listView = findViewById(R.id.lvGameList);
 
-        ArrayList<Game> games = new ArrayList<>();// populate your list of games here
-        games.add(new Game("Game 1", "Description 1", R.drawable.game_item_bg_01));
-        games.add(new Game("Game 2", "Description 2", R.drawable.game_item_bg_01));
-        games.add(new Game("Game 3", "Description 3", R.drawable.game_item_bg_01));
-        games.add(new Game("Game 4", "Description 4", R.drawable.game_item_bg_01));
-        games.add(new Game("Game 5", "Description 5", R.drawable.game_item_bg_01));
-        games.add(new Game("Game 6", "Description 6", R.drawable.game_item_bg_01));
-        games.add(new Game("Game 7", "Description 7", R.drawable.game_item_bg_01));
-        games.add(new Game("Game 7", "Description 7", R.drawable.game_item_bg_01));
-        games.add(new Game("Game 7", "Description 7", R.drawable.game_item_bg_01));
-        games.add(new Game("Game 7", "Description 7", R.drawable.game_item_bg_01));
-        games.add(new Game("Game 7", "Description 7", R.drawable.game_item_bg_01));
 
+
+        games = new ArrayList<>();// populate your list of games here
+
+
+        ivAddGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"click!",Toast.LENGTH_SHORT);
+                addGame(0);
+            }
+        });
+    }
+    private void addGame(int index)
+    {
+        games.add(new Game("Game " + index, "Description " + index, R.drawable.game_item_bg_01));
         MyGameAdapter adapter = new MyGameAdapter(this, games);
         listView.setAdapter(adapter);
     }
--- app/src/main/java/com/example/finalproject/GameFragment.java
+++ app/src/main/java/com/example/finalproject/GameFragment.java
@@ -4,10 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+
 import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
 
 public class GameFragment extends Fragment {
 
+    ImageView ivAddGame;
+    ListView listView;
+    ArrayList<Game> games;
     public GameFragment() {
         // Required empty public constructor
     }
@@ -16,5 +24,12 @@ public class GameFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.activity_game, container, false);
+    }
+
+    private void addGame(int index)
+    {
+        games.add(new Game("Game " + index, "Description " + index, R.drawable.game_item_bg_01));
+        MyGameAdapter adapter = new MyGameAdapter(getContext(), games);
+        listView.setAdapter(adapter);
     }
 }
