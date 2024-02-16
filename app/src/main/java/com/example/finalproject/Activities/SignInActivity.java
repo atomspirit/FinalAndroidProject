2c9c3053c1a8aabe357a793968c4deb5600a06b3
atomspirit <Yoavd246@gmail.com>
2024-02-16 13:42:24 +0200

    Refactoring login to signin

--- app/src/main/java/com/example/finalproject/Activities/SignInActivity.java
+++ app/src/main/java/com/example/finalproject/Activities/SignInActivity.java
@@ -5,23 +5,27 @@ import androidx.constraintlayout.motion.widget.MotionLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.finalproject.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     Button btLogin;
     EditText etUsername;
+    TextView tvMoveToSignUp;
     MotionLayout motionLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_in);
 
-        motionLayout = findViewById(R.id.motion_layout_login);
+        initComponents();
+
         motionLayout.setTransitionListener(new MotionLayout.TransitionListener() {
             @Override
             public void onTransitionStarted(MotionLayout motionLayout, int startId, int endId) {
@@ -42,6 +46,17 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onTransitionTrigger(MotionLayout motionLayout, int triggerId, boolean positive, float progress) {
 
+            }
+        });
+    }
+    private void initComponents()
+    {
+        motionLayout = findViewById(R.id.motion_layout_login);
+        tvMoveToSignUp = findViewById(R.id.tvMoveToSignUp);
+        tvMoveToSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
             }
         });
     }
